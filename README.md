ff50708914a7ffa805d17c8290a48083cafb430f
Waine Oliveira Jr <waine@aerosim.io>
2024-06-30 14:36:13 -0300

    Updated source code

--- README.md
+++ README.md
@@ -6,6 +6,22 @@ Este projeto é aberto para toda a comunidade do canal [Waine - Dev do Desempenh
 
 O Acelerado é um bot do Discord criado para notificar sobre novos vídeos do YouTube diretamente no servidor da comunidade. Ele foi desenvolvido utilizando `discord.py`
 
+## Rodando
+
+Para rodar, primeiro instalar as dependências utilizando [poetry](https://python-poetry.org/).
+
+```
+pip install -U poetry
+poetry install
+```
+
+Após isso, copiar o arquivo `.example.env` para `.env`, alterar as variáveis de ambiente.
+Então rodar o bot com
+
+```
+poetry run acelerado
+```
+
 ## Como Contribuir
 
 Todos são bem-vindos para contribuir com o projeto. Siga as instruções abaixo para começar:
@@ -34,16 +50,20 @@ Envie suas alterações:
 Abra um Pull Request no repositório original.
 
 ## Relatar Problemas
+
 Caso encontre algum bug, por favor, abra uma issue detalhando o máximo possível o erro encontrado. Isso nos ajudará a identificar e corrigir o problema mais rapidamente.
 
 ## Links Importantes
+
 [Canal do YouTube](https://www.youtube.com/@waine_jr).
 [Servidor do Discord](https://discord.gg/RHuhFcfzyV).
 
 ## Licença
-Este projeto é licenciado sob a MIT License - veja o arquivo LICENSE para mais detalhes.
+
+Este projeto é licenciado sob a [MIT License](./LICENSE).
 
 ## Contato
+
 Para dúvidas ou mais informações, entre em contato pelo servidor do Discord ou comente no canal do YouTube.
 
 Obrigado por fazer parte da nossa comunidade!
---
+++ acelerado/__main__.py
@@ -0,0 +1,91 @@
+import asyncio
+
+from dotenv import dotenv_values
+from googleapiclient.discovery import build
+
+import discord as disc
+from discord.ext import commands
+
+config = dotenv_values(".env")
+required_keys = (
+    "DISCORD_CHANNEL_ID",
+    "YOUTUBE_CHANNEL_ID",
+    "DISCORD_TOKEN",
+    "YOUTUBE_API_KEY",
+)
+if not all(s in config for s in required_keys):
+    raise KeyError(
+        f"Not all required keys in .env. Keys found {list(config.keys())}. Keys required {required_keys}"
+    )
+
+bot = commands.Bot(command_prefix="/", intents=disc.Intents.default())
+
+# Configurações do bot
+DISCORD_CHANNEL_ID = config["DISCORD_CHANNEL_ID"]
+
+# Configuração da API do YouTube
+youtube = build("youtube", "v3", developerKey=config["YOUTUBE_API_KEY"])
+
+
+# Função para obter o último vídeo do canal do YouTube
+async def get_latest_video():
+    request = youtube.search().list(
+        part="snippet",
+        channelId=config["YOUTUBE_CHANNEL_ID"],
+        maxResults=1,
+        order="date",
+    )
+    response = request.execute()
+    if "items" in response and response["items"]:
+        return response["items"][0]
+    return None
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged on as {bot.user}!")
+    await bot.change_presence(
+        activity=disc.Activity(
+            type=disc.ActivityType.watching, name="Waine - Dev do desemepenho"
+        )
+    )
+
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+        await check_new_videos()
+
+    except Exception as e:
+        print(f"Error Syncing commads: {e}")
+
+
+async def check_new_videos():
+    latest_video_id = None
+    while True:
+        video = await get_latest_video()
+        if video:
+            video_id = video["id"]["videoId"]
+            # Trecho Abaixo usado para testar
+            # if(latest_video_id == None):
+            #    latest_video_id = video_id
+            #    video_title = video['snippet']['title']
+            #    video_url = f'https://www.youtube.com/watch?v={video_id}'
+            #    channel = bot.get_channel(DISCORD_CHANNEL_ID)
+            #    await channel.send(f'Novo vídeo: **{video_title}**\n{video_url}')
+            #
+            if video_id != latest_video_id:
+                latest_video_id = video_id
+                video_title = video["snippet"]["title"]
+                video_url = f"https://www.youtube.com/watch?v={video_id}"
+                channel = bot.get_channel(DISCORD_CHANNEL_ID)
+                await channel.send(f"Novo vídeo: **{video_title}**\n{video_url}")
+        await asyncio.sleep(300)
+
+
+async def main():
+    async with bot:
+        await bot.start(config["DISCORD_TOKEN"])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
