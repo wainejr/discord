a12cb554a67d949ded8281f9d18eb94c3ac9f5b6
Waine Oliveira Jr <waine@aerosim.io>
2024-06-30 23:05:05 -0300

    Updated file

--- acelerado/__main__.py
+++ acelerado/__main__.py
@@ -42,8 +42,32 @@ async def get_latest_video():
     return None
 
 
+latest_video_id = None
+
+async def check_new_videos():
+    global latest_video_id
+    video = await get_latest_video()
+
+    if video:
+        video_id = video["id"]["videoId"]
+        # Trecho Abaixo usado para testar
+        # if(latest_video_id is None):
+        #    latest_video_id = video_id
+        #    video_title = video['snippet']['title']
+        #    video_url = f'https://www.youtube.com/watch?v={video_id}'
+        #    channel = bot.get_channel(DISCORD_CHANNEL_ID)
+        #    print(bot, channel, DISCORD_CHANNEL_ID)
+        #    await channel.send(f'Novo vídeo: **{video_title}**\n{video_url}')
+        if video_id != latest_video_id:
+            latest_video_id = video_id
+            video_title = video["snippet"]["title"]
+            video_url = f"https://www.youtube.com/watch?v={video_id}"
+            channel = bot.get_channel(DISCORD_CHANNEL_ID)
+            await channel.send(f"Novo vídeo: **{video_title}**\n{video_url}")
+
 @bot.event
 async def on_ready():
+    global latest_video_id
     logger.info(f"Logged on as {bot.user}!")
     await bot.change_presence(
         activity=disc.Activity(
@@ -51,37 +75,22 @@ async def on_ready():
         )
     )
 
-    try:
-        synced = await bot.tree.sync()
-        logger.info(f"Synced {len(synced)} command(s)")
-        await check_new_videos()
-    except Exception as e:
-       logger.error(f"Error Syncing commads: {e}")
+    # Update latest video, to not post this one
+    latest_video_id = await get_latest_video()
 
-
-async def check_new_videos():
-    latest_video_id = None
     while True:
-        video = await get_latest_video()
-        if video:
-            video_id = video["id"]["videoId"]
-            # Trecho Abaixo usado para testar
-            # if(latest_video_id is None):
-            #    latest_video_id = video_id
-            #    video_title = video['snippet']['title']
-            #    video_url = f'https://www.youtube.com/watch?v={video_id}'
-            #    channel = bot.get_channel(DISCORD_CHANNEL_ID)
-            #    print(bot, channel, DISCORD_CHANNEL_ID)
-            #    await channel.send(f'Novo vídeo: **{video_title}**\n{video_url}')
+        try:
+            synced = await bot.tree.sync()
+            logger.info(f"Synced {len(synced)} command(s)")
+        except Exception as e:
+            logger.error(f"Error Syncing commads: {e}")
 
-            if video_id != latest_video_id:
-                latest_video_id = video_id
-                video_title = video["snippet"]["title"]
-                video_url = f"https://www.youtube.com/watch?v={video_id}"
-                channel = bot.get_channel(DISCORD_CHANNEL_ID)
-                await channel.send(f"Novo vídeo: **{video_title}**\n{video_url}")
+        try:
+            await check_new_videos()
+        except BaseException as e:
+            logger.error(f"Error Checking new videos: {e}")
+
         await asyncio.sleep(300)
-
 
 async def amain():
     async with bot:
